691c7ff2c7382a58c46185fcdd33e321eed2a316
niwdE293 <edwin.adel@elev.ga.ntig.se>
2026-04-17 14:22:20 +0200

    2026-04-17

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 document.querySelector(".fade-layer").addEventListener("click", showMenu)
 
 document.querySelector(".menu-button").addEventListener("click", showMenu)
+document.querySelector(".menu-button-menu").addEventListener("click", showMenu)
 
 
 function showMenu(){
-    let menu = document.querySelector("nav.menu")
+    console.log("hhgfdscfvgbhg")
+    let menu = document.querySelector(".menu")
     menu.classList.toggle("show")
     let layer = document.querySelector(".fade-layer")
     layer.classList.toggle("visible")
